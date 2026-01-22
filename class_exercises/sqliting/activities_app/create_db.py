2d08a81fbde903d6c8492035066706632847f6fe
oweche0809 <oweche0809@highgateschool.org.uk>
2026-01-22 10:19:52 +0000

    db

--- class_exercises/sqliting/activities_app/create_db.py
+++ class_exercises/sqliting/activities_app/create_db.py
@@ -1,31 +1,8 @@
 from sqlalchemy import create_engine
-
-from sqlalchemy.orm import Session
-
-from models import Person, Activity
-
-
-# Create some instances of the Person class
-
-people = [Person(first_name="Andrew", last_name="Dales"),
-        Person(first_name="Chris", last_name="Brolin"),
-        Person(first_name='Vera', last_name="Malcova"),
-]
-
-
-chess = Activity(name="Chess")
-fives = Activity(name="Fives")
-outdoor_ed = Activity(name="Outdoor Ed")
-
-people[0].activities.append(chess)
-people[0].activities.append(fives)
-people[1].activities.append(outdoor_ed)
-people[1].activities.append(fives)
-
-# Connect to the activities database
+from models import Base
 engine = create_engine('sqlite:///activities.sqlite', echo=True)
-# Create a session and add the people to the database
 
-with Session(engine) as sess:
-    sess.add_all(people)
-    sess.commit()
+# drop any existing tables.
+
+Base.metadata.drop_all(engine) # create new tables according to the tables in Base
+Base.metadata.create_all(engine)
